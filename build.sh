aa0c4ac58063728cd3c77b630242a3cd92742e04
zhuangguangtuan <zhuangguangtuan@gmail.com>
2026-04-19 18:31:06 +0800

    🚀 Modify ./build.sh script

--- build.sh
+++ build.sh
@@ -1,14 +1,1 @@
-#!/usr/bin/env bash
-set -e
-
-app=wakatime-sync
-VERSION=$(hatch version)
-
-echo "Version check passed: $VERSION"
-echo "Building ${app}:${VERSION}"
-
-docker build \
-  --build-arg VERSION="$VERSION" \
-  -t "${app}:${VERSION}" \
-  -t "${app}:latest" \
-  .
+docker compose build
